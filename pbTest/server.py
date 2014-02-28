8b9b5a2548f182539f475181e826db90af7784a9
Sam Rendall <sdrendall@gmail.com>
2014-02-27 22:32:36 -0500

    Substituded ServerRoot, RaspberryPiConnectionProtocol and RaspberryPiServerFactory classes into server.py

--- pbTest/server.py
+++ pbTest/server.py
@@ -6,11 +6,64 @@
 from twisted.spread import pb
 from twisted.internet import reactor
 
-class One(pb.Root):
+class ServerRoot(pb.Root):
+
+    clientNames = []
+    clientRefs = {}
+
+    def remote_success(self):
+        print "Success!"
+
+    def remote_registerPi(self, name, ref):
+        self.clientNames.append(name)
+        self.clientRefs[name] = ref
+
+        # Some stuff to see if it worked. For Debugging
+        print self.clientNames
+        self.clientRefs[name].callRemote(success)
+
+  
+    ## FROM THE TUTORIAL!!      
     def remote_takeTwo(self, two):
         print "received a Two called", two
         print "telling it to print(12)"
         two.callRemote("print", 12)
 
-reactor.listenTCP(8800, pb.PBServerFactory(One()))
+
+# Is instantiated/run for each connected Raspberry Pi
+# Using Perspective Broker (PB) now instead of LineReceiver
+class RaspberryPiConnectionProtocol(pb.Broker):
+
+    clientName = None
+    idNo = None
+
+    #ip = None
+    #role = ""    
+    def connectionMade(self):
+        print "Connection Made!"
+
+    # Stores the the hostname of the connected client in self.clientName
+    def setClientName(self, name):
+        print "recieved:", result
+        self.clientName = name
+        print "Client No:", self.idNo, "Is named:", self.clientName
+
+
+# Handles Connections from new Raspberry Pis
+class RaspberryPiServerFactory(pb.PBServerFactory):
+
+    protocol = RaspberryPiConnectionProtocol
+    currentProtocolId = 0
+
+    def getNextProtocolId(self):
+        self.currentProtocolId += 1
+        return self.currentProtocolId
+
+    def buildProtocol(self, addr):
+        print "Building Protocol!"
+        proto = pb.PBServerFactory.buildProtocol(self, addr)
+        proto.idNo = self.getNextProtocolId()
+        return proto
+
+reactor.listenTCP(8800, RaspberryPiServerFactory(ServerRoot()))
 reactor.run()
