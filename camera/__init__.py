b1da48279a25fae4824f3992b558cbecd0277510
Sam Rendall <sdrendall@gmail.com>
2014-03-12 16:49:44 -0400

    Created camera package

---
+++ camera/__init__.py
@@ -0,0 +1,3 @@
+# This is the init file for Sam Rendall's camera package, part of the Gray Lab conditioning cage project 
+# This package contains modules that are to be used to control data aquisition using raspberry pi camera boards
+__all__ = ["rpiCameraControls"]
---
+++ camera/test.py
@@ -0,0 +1,9 @@
+
+class MyClass(object):
+    value = None 
+
+myObj = MyClass()
+
+myObj.value = 5
+
+print myObj.value
